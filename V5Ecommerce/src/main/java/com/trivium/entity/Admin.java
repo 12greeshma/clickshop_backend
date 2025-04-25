9caa7d62d88321b1198399308c311795bd88e6c8
ecgreeshma <greeshmachitrala@gmail.com>
2025-04-25 10:49:38 +0530

    first commit

---
+++ V5Ecommerce/src/main/java/com/trivium/entity/Admin.java
@@ -0,0 +1,54 @@
+package com.trivium.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Admin {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+    private String password;
+  
+    private String role = "ROLE_ADMIN";
+
+	public Admin(Long id, String email, String password, String role) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+	public Admin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+    // getters and setters
+    
+}
